99555740391d6dcff43d7e2a5f4da71e9a6af8a4
Ivan Roseland <ivan.roseland@mitel.com>
2019-01-08 16:45:21 -0600

    boom

---
+++ manifests/site.pp
@@ -0,0 +1,5 @@
+# site.pp for initial puppet environment, really we should think
+# of this as the puppet infrastructure environment.  This requires include classes
+# because we are so low in the stack that we cannot
+# realisticaly expect to have any useful classification going on
+hiera_include('classes', undef)
